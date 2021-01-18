0d2a28ec13a5c74cdfdbad4b8950d98a1f0b99a7
stigkristiansen <stigkristiansen@outlook.com>
2021-01-18 15:57:08 +0100

    .

--- libs/autoload.php
+++ libs/autoload.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+include_once __DIR__ . '/traits.php';
+
 foreach (glob(__DIR__ . '/*.php') as $filename) {
     if (basename($filename) != 'autoload.php') {
         include_once $filename;
