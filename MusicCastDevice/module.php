6dbaf0d37339cf42d7e537d88169a5f778a56350
stigkristiansen <stigkristiansen@outlook.com>
2021-01-26 15:00:20 +0100

    .

--- MusicCastDevice/module.php
+++ MusicCastDevice/module.php
@@ -32,6 +32,8 @@
 	
 			$this->RegisterVariableBoolean('Mute', 'Mute', '~Switch', 7);
 			$this->EnableAction('Mute');
+
+			$this->RegisterTimer('Update', 1000, 'YMC_UpdatePlayInfo('.$this->InstanceId.'');');
 			
 		}
 
