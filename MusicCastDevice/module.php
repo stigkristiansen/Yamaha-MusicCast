89744c03b775dac0a455a0fda1ace7c72de102ad
stigkristiansen <stigkristiansen@outlook.com>
2021-02-02 15:30:54 +0100

    Update module.php

--- MusicCastDevice/module.php
+++ MusicCastDevice/module.php
@@ -102,6 +102,7 @@
 							case 3:
 								$this->SetValueEx('Control', 3);
 								self::Playback(PlaybackState::STOP);
+								self::Update(false);
 								break;
 							case 4:
 								$this->SetValueEx('Control', 1);
@@ -175,7 +176,7 @@
 			}
 		}
 
-		public function Update() {
+		public function Update(bool $timer=true) {
 			$ipAddress = $this->ReadPropertyString('IPAddress');
 			if(strlen($ipAddress)>0) {
 				$system = new System($ipAddress);
@@ -183,7 +184,7 @@
 				
 				$status = $zone->Status();
 
-				if($status->power=='on') {
+				if($status->power=='on' && $timer) {
 					$netUSB = new NetUSB($system);
 					$playInfo = $netUSB->PlayInfo();
 
