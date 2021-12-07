86e5cd607de5d2cd5761ac4fb15390c7e7f189c0
stigkristiansen <stigkristiansen@outlook.com>
2021-12-07 23:43:27 +0100

    .

--- MusicCastDicovery/module.php
+++ MusicCastDicovery/module.php
@@ -131,8 +131,8 @@ declare(strict_types=1);
 
 			IPS_LogMessage('MusicCast', $newDevices);
             
-			$this->UpdateFormField('configurator', 'values', $newDevices);
-            $this->UpdateFormField('searchingInfo', 'visible', false);
+			$this->UpdateFormField('Configurator', 'values', $newDevices);
+            $this->UpdateFormField('SearchingInfo', 'visible', false);
 
 			$this->SendDebug(__FUNCTION__, 'Updating form completed', 0);
 
