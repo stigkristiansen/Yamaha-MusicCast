c0f2be0186cc6d2ea7a30b3e3318fb1834d0a918
stigkristiansen <stigkristiansen@outlook.com>
2022-04-13 21:54:23 +0200

    Update module.php

--- MusicCastDevice/module.php
+++ MusicCastDevice/module.php
@@ -451,7 +451,7 @@ class MusicCastDevice extends IPSModule {
 		else
 			$this->SetTimerInterval(Timers::UPDATELISTS . (string) $this->InstanceID, 0);
 		
-		$this->SetTimerInterval(Timers::UPDATE  . (string) $this->InstanceID, 10000);
+		$this->SetTimerInterval(Timers::UPDATE  . (string) $this->InstanceID, 9500);
 	}
 
 	private function StartLink(int $RoomIndex) {
