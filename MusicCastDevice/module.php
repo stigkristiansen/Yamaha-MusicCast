bd6e20b0dc907002d4d176304b0dde0a736275a5
stigkristiansen <stigkristiansen@outlook.com>
2022-04-20 20:03:42 +0200

    Update module.php

--- MusicCastDevice/module.php
+++ MusicCastDevice/module.php
@@ -502,24 +502,13 @@ class MusicCastDevice extends IPSModule {
 					$this->SetValueEx(Variables::ALBUMART_IDENT, '');
 					$this->SetValueEx(Variables::TOTALTIME_IDENT, '');
 					$this->SetValueEx(Variables::PLAYTIME_IDENT, '');
-					$this->SetValueEx(Variables::POSITION_IDENT, 0);
 				} else {
 					$this->SetValueEx(Variables::INPUT_IDENT, $playInfo->Input());
 					$this->SetValueEx(Variables::ARTIST_IDENT, $playInfo->Artist());
 					$this->SetValueEx(Variables::TRACK_IDENT, $playInfo->Track());
 					$this->SetValueEx(Variables::ALBUM_IDENT, $playInfo->Album());
 					$this->SetValueEx(Variables::ALBUMART_IDENT, $playInfo->AlbumartURL());
-					$this->SetValueEx(Variables::TOTALTIME_IDENT, $this->SecondsToString($playInfo->TotalTime()));
-					$this->SetValueEx(Variables::PLAYTIME_IDENT, $this->SecondsToString($playInfo->PlayTime()));
-
-					if($playInfo->TotalTime()>0) {
-						$position = (int)ceil((float)($playInfo->PlayTime()/$playInfo->TotalTime()*100));
-					} else {
-						$position=0;
-					}
-		
-					$this->SetValueEx(Variables::POSITION_IDENT, $position);
-
+								
 					if($this->Lock(Variables::TOTALTIME_IDENT)) {
 						$this->SetBuffer(Variables::TOTALTIME_TEXT, serialize($playInfo->TotalTime()));
 						$this->Unlock(Variables::TOTALTIME_IDENT);
