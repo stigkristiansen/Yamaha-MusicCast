1281cf52b4585a86d04076dc9eadeae6029d340f
stigkristiansen <stigkristiansen@outlook.com>
2022-04-28 23:13:51 +0200

    Update module.php

--- MusicCastDevice/module.php
+++ MusicCastDevice/module.php
@@ -210,8 +210,9 @@ class MusicCastDevice extends IPSModule {
 					case Variables::CONTROL_IDENT:
 						$this->SendDebug(__FUNCTION__, "It's a control ident", 0);
 						$this->SetTimerInterval(Timers::RESETCONTROL . (string) $this->InstanceID, 2000);
-						$state = $this->MapPlaybackState($Value);
+						//$state = $this->MapPlaybackState($Value);
 						//$this->Playback($state);		
+						$state = 'stop';
 						$this->SendDebug(__FUNCTION__, "Handling the control ident is finished. The state was set to " . $state, 0);	
 						break;
 					case Variables::SLEEP_IDENT:
