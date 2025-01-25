a3047872d60af279be27477f0d5fa9c24d3a1cc9
stigkristiansen <stigkristiansen@outlook.com>
2025-01-25 23:59:49 +0100

    Update traits.php

--- libs/traits.php
+++ libs/traits.php
@@ -58,7 +58,7 @@ trait MusicCast {
                 return PlaybackState::PREVIOUS;
             case PlaybackState::NEXT_ID:
                 return PlaybackState::NEXT;
-            case PlaybackState::NOTHING_ID
+            case PlaybackState::NOTHING_ID:
                 return PlaybackState::NOTHING;
             default:
                 return PlaybackState::ERROR;
